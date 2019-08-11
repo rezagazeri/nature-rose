e38be5509f302b8837880a21bed91fb09aa4a187
rezagazeri <rg_320@yahoo.com>
2019-08-11 22:51:49 +0430

    create sass structure and add classname tools

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './scss/utils/app.scss';
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>salam</h1>
     </div>
   );
 }
---
+++ src/components/Toolbar/ToolBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class ToolBar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
